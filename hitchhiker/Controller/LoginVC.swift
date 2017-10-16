81b6fc5a4cff435ecd14948248ebdaeafff21b05
Ken Pierce <kpierce8@gmail.com>
2017-10-15 17:47:17 -0700

    finished 8, everything working except rounded corners on text field and the entry rect stuff. maybe a constraint problem somewhere?

--- hitchhiker/Controller/LoginVC.swift
+++ hitchhiker/Controller/LoginVC.swift
@@ -13,11 +13,20 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.bindtoKeyboard()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleScreenTap(sender:)))
+        self.view.addGestureRecognizer(tap)
     }
 
-   
+    @objc func handleScreenTap(sender:UITapGestureRecognizer) {
+            self.view.endEditing(true)
+    }
+    
     @IBAction func CancelBtnWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    
     
 }
