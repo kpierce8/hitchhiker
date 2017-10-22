6f72ccfffeed6637df14dcee1da2284aa3a6c365
Ken Pierce <kpierce8@gmail.com>
2017-10-22 11:34:00 -0700

    core location operating, super cool, includes center map button, moved location from my inserted one to the simulator apple location

--- hitchhiker/Controller/HomeVC.swift
+++ hitchhiker/Controller/HomeVC.swift
@@ -9,12 +9,16 @@
 import UIKit
 import MapKit
 import RevealingSplashView
+import CoreLocation
 
 
 class HomeVC: UIViewController, MKMapViewDelegate {
 
     @IBAction func menuButtonWasPressed(_ sender: Any) {
         delegate?.toggelLeftPanel()
+    }
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        centerMapOnUserLocation()
     }
     @IBOutlet weak var actionButton: RoundedShadowButton!
     
@@ -24,27 +28,62 @@ class HomeVC: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapview: MKMapView!
     
     var delegate: CenterVCDelegate?
+    var manager: CLLocationManager?
+    var regionRadius: CLLocationDistance = 1000
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+
+
+            manager?.startUpdatingLocation()
+        } else {
+            manager?.requestAlwaysAuthorization()
+        }
+    }
+    
+    
+    func centerMapOnUserLocation() {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(mapview.userLocation.coordinate, regionRadius * 2.0 , regionRadius * 2.0)
+            mapview.setRegion(coordinateRegion, animated: true)
+    }
     
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white )
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapview.delegate = self
+        manager = CLLocationManager()
         
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        
+        
+        checkLocationAuthStatus()
+        mapview.delegate = self
+        centerMapOnUserLocation()
         self.view.addSubview(revealingSplashView)
         revealingSplashView.animationType = SplashAnimationType.heartBeat
         revealingSplashView.startAnimation()
         
         revealingSplashView.heartAttack = true
         
-        // Stuff I added to change starting position
-        let noLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 47.016, longitude: -122.7853)
-        let viewRegion : MKCoordinateRegion  = MKCoordinateRegionMakeWithDistance(noLocation, 10000, 10000)
-        let adjustedRegion: MKCoordinateRegion  = self.mapview.regionThatFits(viewRegion)
-        self.mapview.setRegion(adjustedRegion, animated: true)
-        self.mapview.showsUserLocation = true
+        // Stuff I added to change starting position (removed for lecture 14)
+//        let noLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 47.016, longitude: -122.7853)
+//        let viewRegion : MKCoordinateRegion  = MKCoordinateRegionMakeWithDistance(noLocation, 10000, 10000)
+//        let adjustedRegion: MKCoordinateRegion  = self.mapview.regionThatFits(viewRegion)
+//        self.mapview.setRegion(adjustedRegion, animated: true)
+//        self.mapview.showsUserLocation = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 }
 
+extension HomeVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+         checkLocationAuthStatus()
+        if status == .authorizedAlways {
+
+            mapview.showsUserLocation = true
+            mapview.userTrackingMode = .follow
+        }
+    }
+}
